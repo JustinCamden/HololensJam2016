1f2e1f4062d9845022e6885bb0e1e3ff14e02791
DESKTOP-LO495TT\max13 <jrehberg@usc.edu>
2016-11-05 11:53:29 -0700

    Working

--- Assets/gameManager.cs
+++ Assets/gameManager.cs
@@ -3,14 +3,62 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class gameManager : MonoBehaviour {
+    int gameState;
+    const int BUILDING = 1;
+    const int FIGHTING = 2;
+    const int WIN = 3;
+    const int SCANNING =0;
+ 
+    public float fightTimer = 60f;
+    float currfightTimer;
+
+    public GameObject ghostPillow;
 
 	// Use this for initialization
 	void Start () {
-		
+        gameState = SCANNING;
+        currfightTimer = fightTimer;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+    // Update is called once per frame
+    void Update() {
+        switch (gameState) {
+            case SCANNING:
+                {
+                    ghostPillow.active = false;
+                  
+                    break;
+                }
+            case BUILDING:
+                {
+                    ghostPillow.active = true;
+                    ghostPillow.transform.SetParent(null);
+                    break;
+                }
+
+        }
 	}
+
+    public void switchState()
+    {
+        switch (gameState)
+        {
+            case SCANNING:
+                {
+                    gameState = BUILDING;
+                    break;
+                }
+            case BUILDING:
+                {
+                    gameState = FIGHTING;
+                    break;
+                }
+            case FIGHTING:
+                {
+                    gameState = WIN;
+                    break;
+
+                }
+        }
+    }
 }
