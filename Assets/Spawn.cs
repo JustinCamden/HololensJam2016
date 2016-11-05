36540b3413e0f35c1966726dd843aa354f2ad343
DESKTOP-LO495TT\max13 <jrehberg@usc.edu>
2016-11-04 22:14:08 -0700

    John's Stuff

--- Assets/Spawn.cs
+++ Assets/Spawn.cs
@@ -4,19 +4,19 @@ using UnityEngine;
 
 public class Spawn : MonoBehaviour {
     public GameObject pillow;
-
-	// Use this for initialization
-	void Start () {
-		
+    int counter = 0;
+    // Use this for initialization
+    void Start () {
+        
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        counter++;
 	}
 
     public void spawnPillow()
     {
-        Instantiate(pillow, new Vector3(0, 0, 0), Quaternion.identity);
+        Instantiate(pillow, new Vector3(0, 0, 0), new Quaternion(0,0,0,0));
     }
 }
