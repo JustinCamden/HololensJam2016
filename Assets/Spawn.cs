3abf457a300e927d2aad075a86dbfb1c3ff7f229
DESKTOP-LO495TT\max13 <jrehberg@usc.edu>
2016-11-04 19:23:33 -0700

    Prefabs

---
+++ Assets/Spawn.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour {
+    public GameObject pillow;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void spawnPillow()
+    {
+        Instantiate(pillow, new Vector3(0, 0, 0), Quaternion.identity);
+    }
+}
